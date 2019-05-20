80f6f6657a891b56259a8573421c87ed6ec631b4
Ryan Houser <r.houser90@gmail.com>
2019-05-19 20:01:22 -0400

    Generate random phrases

--- js/Game.js
+++ js/Game.js
@@ -8,21 +8,22 @@
 class Game {
   constructor() {
     this.missed = 0;
-    this.phrases = this.createPhrases();
-    this.activePhrase = null;
-  }
-  
-  /**
-  * Creates phrases for use in game
-  * @return {array} An array of phrases that could be used in the game
-  */
-  createPhrases() {
-    return [
+    this.phrases = [
       'When the Rubber Hits the Road',
       'Throw In the Towel',
       'Mountain Out of a Molehill',
       'Fish Out Of Water',
       'Head Over Heels'
-    ]
+    ];
+    this.activePhrase = null;
+  }
+
+  /**
+   * Selects random phrase from phrases property
+   * @return {Object} Phrase object chosen to be used
+   */
+  getRandomPhrase() {
+    const random = Math.floor(Math.random() * this.phrases.length);
+    return this.phrases[random];
   }
 }
